e2339e318822f19ec4e78e734a82098875529da1
MAMV3x3 <mamv3x3@gmail.com>
2023-01-20 01:56:09 -0600

    Landing page updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import "./Home.css"
+import "./Home.css";
+import SocialsHeader from "./SocialsHeader";
+import ScrollDown from "./ScrollDown";
+import Avatar from "../../assets/logo.svg"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Avatar} alt="avatar" className="home__img" />
+        <h1 className="home__name">Miguel Ángel Mireles Vázquez</h1>
+        <span className="home__education">Mechatronics Engineer</span>
+
+        <SocialsHeader />
+
+        <a href="#contact" className="btn">Hire me</a>
+
+        <ScrollDown />
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/home/SocialsHeader.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const SocialsHeader = () => {
+  return (
+    <div className="home__socials">
+        <a href="a" className="home__socials-link" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="a" className="home__socials-link" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href="a" className="home__socials-link" target="_blank">
+            <i class="fa-brands fa-github"></i>
+        </a>
+
+        <a href="a" className="home__socials-link" target="_blank">
+            <i class="fa-brands fa-facebook"></i>
+        </a>
+
+        <a href="a" className="home__socials-link" target="_blank">
+            <i class="fa-brands fa-discord"></i>
+        </a>
+    </div>
+  )
+}
+
+export default SocialsHeader
