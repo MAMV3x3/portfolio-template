918e8cdb82fe3ed3c2104b754e630bc7ecb30602
MAMV3x3 <mamv3x3@gmail.com>
2023-01-20 00:30:25 -0600

    Sidebar component updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import "./Sidebar.css"
+import Logo from "../../assets/logo.svg"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className="aside">
+      <a href="#home" className="nav__logo">
+        <img src={Logo} alt="logo" />
+      </a>
+
+      <nav className="nav">
+        <div className="nav__menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="icon-home"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="icon-user-following"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="icon-briefcase"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#resume" className="nav__link">
+                <i className="icon-graduation"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="icon-layers"></i>
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="icon-bubble"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="nav__footer">
+        <span className="copyright">&copy; 2023 - 2024.</span>
+      </div>
+    </aside>
   )
 }
 
