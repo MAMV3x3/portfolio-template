76c56cf4113aec4fcfa701196dd4083319120060
MAMV3x3 <mamv3x3@gmail.com>
2023-01-20 02:54:04 -0600

    Resume page added

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import "./Resume.css"
+import Data from './Data';
+import Card from './Card';
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <section className="resume container section" id="resume">
+      <h2 className="section__title">Resume</h2>
+
+      <div className="resume__container grid">
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            if (val.category === "education") {
+              return (
+                <Card key={id} icon={val.icon} title={val.title} year={val.year} institution={val.institution} desc={val.desc}/>
+              )
+            }
+          })}
+        </div>
+
+        <div className="timeline grid">
+          {Data.map((val, index) => {
+            if (val.category === "experience") {
+              return (
+                <Card key={index} icon={val.icon} title={val.title} year={val.year} institution={val.institution} desc={val.desc}/>
+              )
+            }
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
