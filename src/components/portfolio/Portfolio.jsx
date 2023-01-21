17b0faa2c1343fb0d29b554b1c766e88bdf66be0
MAMV3x3 <mamv3x3@gmail.com>
2023-01-21 00:34:58 -0600

    Fixed projects tab typo

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,7 +7,7 @@ const Portfolio = (props) => {
     <section className="portfolio container section" id="resume">
       <div className="portfolio__container">
         <div className="tags top-tags">&lt;h2&gt;</div>
-          <h2 className="section__title">Proyects</h2>
+          <h2 className="section__title">Projects</h2>
         <div className="tags bottom-tags">&lt;/h2&gt;</div>
           <HoriziontalList proyects ={props.proyects}/>
       </div>
