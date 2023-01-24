9cd37881fa06278d865d92c2ece64b86c0e943ac
MAMV3x3 <mamv3x3@gmail.com>
2023-01-24 01:37:50 -0600

    Added shapes and parallax effect to landing page

---
+++ src/components/text-layout/Text.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Text.css"
+
+const Text = () => {
+  return (
+    <div className="transparen-text">
+        <h1 className="transparent-text__heading">
+          de <br /> ve <br /> lo <br /> pe <br /> rs
+        </h1>
+    </div>
+  )
+}
+
+export default Text
